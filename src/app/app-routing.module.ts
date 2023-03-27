b84cab086346311667e51465ac0e483a89b59b62
Susmita <susmitajadhav01@gmail.com>
2023-03-28 04:07:13 +0530

    Version common 10 to 11 update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -38,9 +38,8 @@ const routes: Routes = [
 
 ];
 
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
